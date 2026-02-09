ee49313ca73437e0a2ec7f40a82c48c57f17c6bd
Sohail3244 <sohailahmedmaniyar@gmail.com>
2026-02-09 12:21:17 +0530

    add

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,5 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss(),],
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: [
+      'jwokgk8c00csoko44ks444sw.147.93.20.127.sslip.io'
+    ]
+  }
 })
